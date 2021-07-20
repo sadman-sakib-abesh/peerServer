e3584f461783afb3bc9ec56dc745f9eeb5137492
sadman-sakib-abesh <63991795+sadman-sakib-abesh@users.noreply.github.com>
2021-07-20 18:47:24 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+var ExpressPeerServer = require("peer").ExpressPeerServer;    
+var options = {
+  debug: true,
+  allow_discovery: true,
+};
+let peerServer = ExpressPeerServer(server, options);
+app.use("/peerjs", peerServer);
